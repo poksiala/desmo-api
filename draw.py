09af92f1ca689ffef971d5653646c71e6cde8a10
poksiala <panu@oksiala.fi>
2024-08-24 20:23:16 +0300

    Cleanup

--- draw.py
+++
@@ -1,9 +0,0 @@
-from statemachine.contrib.diagram import DotGraphMachine
-from desmo_api.fsm import JailStateMachine
-
-
-if __name__ == "__main__":
-    graph = DotGraphMachine(JailStateMachine)
-    graph.graph_rankdir = "TB"
-    dot = graph()
-    dot.write_png("fsm-desmo.png")
